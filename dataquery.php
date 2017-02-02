9477d860fe00d4fc0e69fc387a1a414923599cf2
Elliot Algase <mostdigitsofpi@gmail.com>
2017-02-02 07:01:48 -0500

    make some changes

--- dataquery.php
+++ dataquery.php
@@ -4,5 +4,27 @@
 
 <?php
 
+class DB extends SQLite3
+{
+        function __construct( $file )
+        {
+            $this->open( $file );
+        }
+}
 
+$db = new DB( '/var/www/html/VideoGameFinder/games.db' );
+
+
+$rows = $db->query("SELECT COUNT(*) as count FROM games");
+$row = $rows->fetchArray();
+$numRows = $row['count'];
+echo $numRows;
+$i=1;
+while ($i<=$numRows){
+echo "<img src='";
+echo $db->querySingle("SELECT image FROM games WHERE id=" . $i);
+echo "' width='100' height='100'>";
+$i+=1;
+}
+echo $db->querySingle("SELECT image FROM games");
 ?>
