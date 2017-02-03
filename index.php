e62af4bf0deb81d7b4b00d21ad8bc01775224484
MCJack123 <jackbruienne@yahoo.com>
2017-02-03 09:51:36 -0500

    Update index.php

--- index.php
+++ index.php
@@ -21,43 +21,47 @@
 	</h3>
 	<form action='type1_redir.php' method='GET'>
 	<span>Lowest Age Rating</span><br>
-	<input type='radio' name='agerangelow' value='1' />1 <br>
-	<input type='radio' name='agerangelow' value='2' />2 <br>
-	<input type='radio' name='agerangelow' value='3' />3 <br>
-	<input type='radio' name='agerangelow' value='4' />4 <br>
-	<input type='radio' name='agerangelow' value='5' />5 <br>
-	<input type='radio' name='agerangelow' value='6' />6 <br>
-	<input type='radio' name='agerangelow' value='7' />7 <br>
-	<input type='radio' name='agerangelow' value='8' />8 <br>
-	<input type='radio' name='agerangelow' value='9' />9 <br>
-	<input type='radio' name='agerangelow' value='10' />10 <br>
-	<input type='radio' name='agerangelow' value='11' />11 <br>
-	<input type='radio' name='agerangelow' value='12' />12 <br>
-	<input type='radio' name='agerangelow' value='13' />13 <br>
-	<input type='radio' name='agerangelow' value='14' />14 <br>
-	<input type='radio' name='agerangelow' value='15' />15 <br>
-	<input type='radio' name='agerangelow' value='16' />16 <br>
-	<input type='radio' name='agerangelow' value='17' />17 <br>
-	<input type='radio' name='agerangelow' value='18' />18 <br>
+	<select name="agerangelow">
+	<option value='1'>1</option>
+	<option value='2'>2</option>
+	<option value='3'>3</option>
+	<option value='4'>4</option>
+	<option value='5'>5</option>
+	<option value='6'>6</option>
+	<option value='7'>7</option>
+	<option value='8'>8</option>
+	<option value='9'>9</option>
+	<option value='10'>10</option>
+	<option value='11'>11</option>
+	<option value='12'>12</option>
+	<option value='13'>13</option>
+	<option value='14'>14</option>
+	<option value='15'>15</option>
+	<option value='16'>16</option>
+	<option value='17'>17</option>
+	<option value='18'>18</option>
+	</select>
 	<span>Highest Age Rating</span><br>
-        <input type='radio' name='agerangehigh' value='1' />1 <br>
-        <input type='radio' name='agerangehigh' value='2' />2 <br>
-        <input type='radio' name='agerangehigh' value='3' />3 <br>
-        <input type='radio' name='agerangehigh' value='4' />4 <br>
-        <input type='radio' name='agerangehigh' value='5' />5 <br>
-        <input type='radio' name='agerangehigh' value='6' />6 <br>
-        <input type='radio' name='agerangehigh' value='7' />7 <br>
-        <input type='radio' name='agerangehigh' value='8' />8 <br>
-        <input type='radio' name='agerangehigh' value='9' />9 <br>
-        <input type='radio' name='agerangehigh' value='10' />10 <br>
-        <input type='radio' name='agerangehigh' value='11' />11 <br>
-        <input type='radio' name='agerangehigh' value='12' />12 <br>
-        <input type='radio' name='agerangehigh' value='13' />13 <br>
-        <input type='radio' name='agerangehigh' value='14' />14 <br>
-        <input type='radio' name='agerangehigh' value='15' />15 <br>
-        <input type='radio' name='agerangehigh' value='16' />16 <br>
-        <input type='radio' name='agerangehigh' value='17' />17 <br>
-        <input type='radio' name='agerangehigh' value='18' />18+ <br>
+	<select name="agerangehigh">
+        <option value='1'>1</option>
+	<option value='2'>2</option>
+	<option value='3'>3</option>
+	<option value='4'>4</option>
+	<option value='5'>5</option>
+	<option value='6'>6</option>
+	<option value='7'>7</option>
+	<option value='8'>8</option>
+	<option value='9'>9</option>
+	<option value='10'>10</option>
+	<option value='11'>11</option>
+	<option value='12'>12</option>
+	<option value='13'>13</option>
+	<option value='14'>14</option>
+	<option value='15'>15</option>
+	<option value='16'>16</option>
+	<option value='17'>17</option>
+	<option value='18'>18+</option>
+	</select>
 	<input class='w3-btn w3-blue w3-round-xxlarge' type='submit'>
 	</form>
     </body>
